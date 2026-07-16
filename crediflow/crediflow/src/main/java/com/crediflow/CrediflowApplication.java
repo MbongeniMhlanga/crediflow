a0636382a7f552e66528059af22b50e44f068056
Mbongeni Mhlanga <mbongeniroyce@gmail.com>
2026-07-16 17:47:20 +0200

    springboot app initialization

---
+++ crediflow/crediflow/src/main/java/com/crediflow/CrediflowApplication.java
@@ -0,0 +1,13 @@
+package com.crediflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrediflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrediflowApplication.class, args);
+	}
+
+}
