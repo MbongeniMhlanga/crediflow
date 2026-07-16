e1ce23ac0e6bbaaf331876e6d01bd2cb06831ada
Mbongeni Mhlanga <mbongeniroyce@gmail.com>
2026-07-16 18:28:56 +0200

    Update LoanApplicationService.java

--- crediflow/crediflow/src/main/java/com/crediflow/service/LoanApplicationService.java
+++ crediflow/crediflow/src/main/java/com/crediflow/service/LoanApplicationService.java
@@ -12,7 +12,6 @@ import com.crediflow.util.FinancialCalculationUtil;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
